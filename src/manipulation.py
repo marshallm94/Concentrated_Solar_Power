f7a3eac9170a85f757e0742fc9b9b4892c4d81ad
Marshall McQuillen <marshallm94@gmail.com>
2018-04-15 11:26:30 -0600

    Add build_master_csv()

--- src/manipulation.py
+++ src/manipulation.py
@@ -1,50 +1,38 @@
 import pandas as pd
 import numpy as np
+import os
 
-def count_nans(df, verbose=True):
+def build_master_csv(root, filename):
     """
-    Calculates NaN percentages per column in a pandas DataFrame.
+    Reads all files in filepath specified by root parameter and
+    concatenates into one Pandas DataFrame, which is then saved as a
+    separate file specified by filename parameter
 
     Parameters:
-        df: (Pandas DataFrame)
-        verbose: (Boolean) Prints column names and NaN percentage if True
+        root: (str) The relative or absolute path to the directory containing all files desired to be concatenated.
+        filename: (str) The filename where the final data object should be saved.
 
-    Output:
-        col_nans: List containing tuples of column names and percentage NaN for that column.
+    Returns:
+        None
     """
-    col_nans = []
-    for col in df.columns:
-        percent_nan = pd.isnull(df[col]).sum()/len(pd.isnull(df[col]))
-        col_nans.append((col, percent_nan))
-        if verbose:
-            print("{} | {:.2f}% NaN".format(col, percent_nan*100))
+    all_files = os.listdir(path=root)
+    all_files.remove("header.csv")
+    col_names = list(pd.read_csv("../data/solar_measurements/header.csv"))
+    for x, file in enumerate(all_files):
+        current_file = root + file
+        if x == 0:
+            df = pd.read_csv(current_file, header=None, names=col_names)
+        else:
+            current_df = pd.read_csv(current_file, header=None, names=col_names)
+            df = pd.concat([df, current_df], axis=0)
 
-    return col_nans
+        print(f"Final DataFrame Shape: {df.shape}")
 
-def import_data(root, start, stop):
-    """
-    Import multiple csv files from one directory assuming there is consistency in the naming of the files.
-
-    Parameters:
-        root: (str) The relative or absolute path to the directory containing all files desired.
-        start: (str) The name of the file to import first, in this case a data formatted as YYYYMMDD
-        stop: (str) The name of last file to import, in this case a data formatted as YYYYMMDD
-
-    Return:
-        df: (Pandas DataFrame) One dataframe for all data from all files contained in the date range specified by start and stop parameters.
-    """
-    extension = ".csv"
-    out = root + start + extension
-    print(out)
-
-
+    df.to_csv(filename)
+    print(f"\nAll files concatenated to one data object and saved to {filename}\nProcess Complete.")
 
 if __name__ == "__main__":
 
-    root = "../data/solar_measurements/"
+    build_master_csv("../data/solar_measurements/", "../data/ivanpah_measurements.csv")
 
-    df = import_data(root, "20060318", "20061231")
-
-    col_names = list(pd.read_csv("../data/solar_measurements/header.csv"))
-
-    test = pd.read_csv("../data/solar_measurements/20180413.csv", header=None, names=col_names)
+    df = pd.read_csv("../data/ivanpah_measurements.csv")
