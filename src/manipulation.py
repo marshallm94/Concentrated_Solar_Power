d969b5efea16e4402edb053eb2bc54776536f72a
Marshall McQuillen <marshallm94@gmail.com>
2018-04-16 05:21:49 -0600

    Add hour, minute, date columns in build_master_csv()

--- src/manipulation.py
+++ src/manipulation.py
@@ -19,21 +19,35 @@ def build_master_csv(root, filename):
     all_files = os.listdir(path=root)
     all_files.remove("header.csv")
     col_names = list(pd.read_csv("../data/solar_measurements/header.csv"))
-    for x, file in enumerate(all_files):
-        current_file = root + file
+
+    minutes = np.arange(60)
+    minute_array = np.array([np.copy(minutes) for i in range(24)]).ravel()
+    for x, small_file in enumerate(all_files[:1]):
+        current_file = root + small_file
+        date = small_file[:-4]
+        year, month, day = date[:4], date[4:6], date[6:]
+        date = year + "-" + month + "-" + day
         if x == 0:
             df = pd.read_csv(current_file, header=None, names=col_names)
+            df["Hour"] = df.index // 60
+            df['Minute'] = minute_array
+            df["Date"] = date
         else:
             current_df = pd.read_csv(current_file, header=None, names=col_names)
+            current_df["Hour"] = current_df.index // 60
+            curren_df['Minute'] = minute_array
+            current_df["Date"] = date
             df = pd.concat([df, current_df], axis=0)
 
+        print(df.head())
         print(f"Final DataFrame Shape: {df.shape}")
 
+    return df
     df.to_csv(filename)
     print(f"\nAll files concatenated to one data object and saved to {filename}\nProcess Complete.")
 
 
-def check_data_count(year_list, df, verbose=True):
+def check_data_count(year_list, df):
     """
     Checks to ensure that every yeat in year_list has 355 or 366 days
     and that every day has 1440 observations (1 observation per
@@ -44,12 +58,18 @@ def check_data_count(year_list, df, verbose=True):
         df: (pandas dataframe)
         verbose: (boolean, default=True) Will print output if True
 
-    Return:
+    Returns:
+        list of tuples where the first element in the tuple is the year and the second element is the number of days in that year
     """
+    out = []
+
     for year in year_list:
         subset = df[df["Year"] == year]
-        print("\n\n", str(year), "\n\n")
-        print((subset.groupby("DOY").count() == 1440).sum())
+        days = (subset.groupby("DOY").count() == 1440).sum()[0]
+        out.append((year, days))
+
+    return out
+
 
 def make_dates(year_list, df):
     """
@@ -61,52 +81,55 @@ def make_dates(year_list, df):
     Return:
         updated_df: (pandas dataframe) Dataframe with datetime as index
     """
-    for year in year_list:
-        mask = df['Year'] == 2014
-        days = np.unique(df[mask]['DOY'])
-        for day in days:
-            mask2 = df[mask]['DOY'] == 45
-            hours = df[mask & mask2].reset_index().index // 60
-            for hour in np.unique(hours):
-                np.where(hours == hour, True, False)
-                mask3 = df[mask & mask2]['']
-                minute_subset = minute_subset.reset_index()
-                minute_subset["Minute"] == minute_subset.index
+    year_day_list = check_data_count(year_list, df)
 
+    for pair in year_day_list:
+        for day in range(pair[1]):
+            mask = df['Year'] == pair[0]
+            mask2 = df['DOY'] == day
+            df[mask & mask2]['Hour'] = df[mask & mask2].reset_index().index // 60
 
-
-    for year in year_list:
-        subset = df[df['Year'] == year]
-        days = np.unique(subset['DOY'])
-        for day in days:
-            nested_subset = subset[subset['DOY'] == day]
-            nested_subset = nested_subset.reset_index
-            nested_subset["Hour"] = nested_subset.index // 60
-            hours = np.unique(nested_subset['Hour'])
-            for hour in hours:
-                minute_subset = nested_subset[nested_subset['Hour'] == hour]
-                minute_subset = minute_subset.reset_index()
-                minute_subset["Minute"] == minute_subset.index
-
-
-    df["string_DOY"] = df["Year"].astype(str) + " " +  df["DOY"].astype(str)
-    df["Date"] = pd.to_datetime(df["string_DOY"], format="%Y %j")
-    hour = 0
-
-
-
+    # for year in year_list:
+    #     mask = df['Year'] == 2014
+    #     mask2 = df[mask]['DOY'] == 45
+    #
+    #     for day in days:
+    #         mask2 = df[mask]['DOY'] == 50
+    #         df['Hour'] = df[mask & mask2].reset_index().index // 60
+            # hours = df[mask & mask2].reset_index().index // 60
     return df
+
+            # for hour in np.unique(hours):
+            #     mask3 = np.where(hours == 12, True, False)
+            #     df[mask & mask2 & mask3]
+            #     minute_subset = minute_subset.reset_index()
+            #     minute_subset["Minute"] == minute_subset.index
+
+
+
+    # for year in year_list:
+    #     subset = df[df['Year'] == year]
+    #     days = np.unique(subset['DOY'])
+    #     for day in days:
+    #         nested_subset = subset[subset['DOY'] == day]
+    #         nested_subset = nested_subset.reset_index
+    #         nested_subset["Hour"] = nested_subset.index // 60
+    #         hours = np.unique(nested_subset['Hour'])
+    #         for hour in hours:
+    #             minute_subset = nested_subset[nested_subset['Hour'] == hour]
+    #             minute_subset = minute_subset.reset_index()
+    #             minute_subset["Minute"] == minute_subset.index
 
 if __name__ == "__main__":
 
-    # build_master_csv("../data/solar_measurements/", "../data/ivanpah_measurements.csv")
+    df = build_master_csv("../data/solar_measurements/", "../data/ivanpah_measurements.csv")
 
-    df = pd.read_csv("../data/ivanpah_measurements.csv")
+    # df = pd.read_csv("../data/ivanpah_measurements.csv")
+    #
+    # df.drop('Unnamed: 0.1', axis=1, inplace=True)
 
-    df.drop('Unnamed: 0.1', axis=1, inplace=True)
-
-    years = [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018]
-
-    check_data_count(years, df)
-
-    df = make_dates(df)
+    # years = [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018]
+    #
+    # year_days = check_data_count(years, df)
+    #
+    # df = make_dates(years, df)
