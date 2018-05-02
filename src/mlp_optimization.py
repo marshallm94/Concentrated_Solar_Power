958b0269ff685201beadcfe5d6ff933ad691cb08
Marshall McQuillen <marshallm94@gmail.com>
2018-05-02 04:16:33 -0600

    Remove abstracted MLP builder functions

--- src/mlp_optimization.py
+++ src/mlp_optimization.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error
 from manipulation import get_master_df
-from modeling import create_X_y, test_model, engineer_lagged_DNI_features
+from modeling import create_X_y, test_model, create_lagged_DNI_features
 import datetime
 from keras.models import Sequential
 from keras.layers.core import Activation, Dense
@@ -52,43 +52,6 @@ def build_neural_network(n_predictors=28, hidden_layer_neurons=8, hidden_layer_n
 
     return model
 
-# def build_first_hidden_layer(input_neurons=28, hidden_layer_neurons=8):
-#
-#     model = Sequential()
-#     model.add(Dense(input_dim=n_neurons,
-#                     units=hidden_layer_neurons,
-#                     kernel_initializer='uniform',
-#                     activation='relu'))
-#     return model
-#
-# def build_hidden_layer_n(model, num_neurons):
-#
-#     model.add(Dense(units=num_neurons,
-#                     kernel_initializer='uniform',
-#                     activation='relu'))
-#     return model
-#
-# def build_output_layer(model):
-#
-#     model.add(Dense(unit=1))
-#     optimizer = RMSprop(lr=0.001, rho=0.9, epsilon=None, decay=0.0)
-#     model.compile(optimizer=optimizer,
-#                   loss='mse')
-#
-#     return model
-#
-# def ag_neural_network(n_predictors=28, num_hidden_layers=2, hidden_layer_neuron_list=[8,12]):
-#
-#     input_neurons = n_predictors
-#     model = build_first_hidden_layer(input_neurons)
-#
-#     for i in range(num_hidden_layers):
-#         model = build_hidden_layer_n(model, hidden_layer_neuron_list[i])
-#
-#     model = build_output_layer(model)
-#
-#     return model
-
 
 if __name__ == "__main__":
 
@@ -107,7 +70,7 @@ if __name__ == "__main__":
 
     print("\nData successfully loaded")
 
-    df = engineer_lagged_DNI_features(15, df)
+    df = create_lagged_DNI_features(15, df)
 
     print("\n15 new features successfully engineered")
 
@@ -147,45 +110,48 @@ if __name__ == "__main__":
     n_predictors = len(columns)
     hidden_layer_neurons = [8, 12]
 
-    np.random.seed(10000)
+    seed = 3
+    np.random.seed(seed)
 
     # create parameter lists for GridSearchCV
         # Results:
             # Bach Size = 17
             # Epochs = 38
 
-    # model = KerasRegressor(build_fn=build_neural_network, epochs=38, batch_size=17)
-    #
-    # x = list(np.arange(10, 30, 1))
-    # x1 = list(np.arange(10, 30, 1))
-    #
-    # neural_net_grid_dict = {'hidden_layer_neurons': x,
-    #                         "hidden_layer_neurons1": x1}
-    #
-    # neural_net_grid = GridSearchCV(estimator=model,
-    #                                param_grid=neural_net_grid_dict,
-    #                                scoring='neg_mean_squared_error',
-    #                                verbose=1,
-    #                                n_jobs=-1,
-    #                                cv=3)
-    #
-    #
-    #
-    # mask = df['Date'] == np.random.choice(np.unique(df['Date']))
-    # X, y = create_X_y(df[mask], columns)
-    #
-    # grid_result = neural_net_grid.fit(X, y)
-    #
-    # print(f"\nBest Score: {grid_result.best_score_}")
-    # print(f"\nBest Parameters: {grid_result.best_params_}")
-    # stop_criteria = EarlyStopping(monitor='val_loss', min_delta=0.005)
-    #
+    model = KerasRegressor(build_fn=build_neural_network, epochs=38, batch_size=17)
+
+    x = list(np.arange(10, 101, 10))
+    x1 = list(np.arange(10, 101, 10))
+
+    neural_net_grid_dict = {'hidden_layer_neurons': x,
+                            "hidden_layer_neurons1": x1}
+
+    neural_net_grid = GridSearchCV(estimator=model,
+                                   param_grid=neural_net_grid_dict,
+                                   scoring='neg_mean_squared_error',
+                                   verbose=1,
+                                   n_jobs=-1,
+                                   cv=3)
+
+
+
+    mask = df['Date'] == np.random.choice(np.unique(df['Date']))
+    X, y = create_X_y(df[mask], columns)
+
+    grid_result = neural_net_grid.fit(X, y)
+
+    print(f"\nBest Score: {grid_result.best_score_}")
+    print(f"\nBest Parameters: {grid_result.best_params_}")
+    stop_criteria = EarlyStopping(monitor='val_loss', min_delta=0.001)
+
     network_dict = {'epochs': 38,
                     'batch_size': 17,
                     'shuffle': True,
-                    'validation_split': 0.25
-                    # 'callback': stop_criteria
+                    'validation_split': 0.25,
+                    'callback': stop_criteria
     }
+
+    mlp = build_neural_network(len(columns), 50, 1)
 
     cv_errors, cv_test_periods, cv_train_periods, pm_errors = test_model(mlp, columns, 10, 90, 31, df, network_dict)
 
