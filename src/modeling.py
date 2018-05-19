bb8b2d29d81094182f4943457134350f49948f71
Marshall McQuillen <marshallm94@gmail.com>
2018-05-19 16:21:56 -0600

    test_model() working with RandomForest

--- src/modeling.py
+++ src/modeling.py
@@ -3,14 +3,16 @@ import numpy as np
 import calendar
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
 from manipulation import get_master_df, plot_day
+import train_mlp_base as tmb
+from eda import format_nrel_dataframe
 from datetime import datetime
 from keras.models import Sequential
 from keras.layers.core import Activation, Dense
 from keras.objectives import MSE, MAE
 from keras.callbacks import EarlyStopping
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, cross_validate, train_test_split
 import matplotlib.dates as mdates
 import random
 
@@ -284,6 +286,27 @@ def create_future_target(df, column, shift, row_time_steps):
     return out
 
 
+def sequential_impute(df, col):
+    '''
+    Replaces NaN values with the value most recently seen in the data set.
+    Assumes DataFrame has a sequential aspect to it.
+
+    Parameters:
+    ----------
+    df : (Pandas DataFrame)
+        DataFrame containing col
+    col : (str)
+        The column name within df for which sequential imputing will be
+        performed
+
+    Returns:
+    ----------
+    df : (Pandas DataFrame)
+    '''
+    out = df.copy()
+    values = out[col].values
+
+
 def create_lagged_DNI_features(num_lagged_features, df):
     """
     Creates new variables that have the Direct Normal [W/m^2] value
@@ -320,105 +343,33 @@ def create_lagged_DNI_features(num_lagged_features, df):
     return df
 
 
-def test_model(model, columns, iter, train_duration, test_duration, df, network=False):
-    """
-    A custom model testing function for predicting DNI 15 minutes
-    out from the current time. The model is trained on 90 days worth
-    of data, 1440 observations per day (1 per minute). The model is
-    then tested on the next 30 days immediately after the training
-    days.
-
-    iter random dates will be selected from the df provided. For
-    each of those dates, the training dataset will be that date and
-    the previous 89 days. The testing dataset will be the 30 days
-    immediately following the random date selected (not including
-    that day.)
+def test_model(model, X, y):
+    '''
+    Evaluates the model specified using 5-fold cross validation and tests model
+    on unseen data.
 
     Parameters:
-        model: (sklearn model object) A model object that implements
-               fit() and predict() methods
-        iter: (int) Number of iterations to do (also number of
-                 random days that will be selected)
-        df: (pandas dataframe)
-        train_duration: (int) The number of days the model will be
-                        trained on
-        test_duration: (int) The number of days the model will be
-                        tested on
-        network: (boolean/dict) False if model is not a Neural
-                 Network (default), else a dict with parameters for
-                 the Network to train with.
+    ----------
+    model : (object)
+        Machine Learning object that implements both .fit() and .predict()
+    X : (Pandas DataFrame)
+        Contains attributes on which the model will be built.
+    y : (Pandas Series)
+        Target variable
 
     Returns:
-        rmses: (list) List of Root Mean Squared Errors of model
-                predictions, one value for each iteration.
-
-        test_periods: (list) List of tuples, element 0 being the
-                        start date of the test period, element 1
-                        being the end date of the test period.
-
-        train_periods: (list) List of tuples, element 0 being the
-                        start date of the training period, element
-                        1 being the end date of the training period.
-
-        persistence_model: (list) List of Root Mean Squared Errors
-                            of the Persistence Model, one value for
-                            each iteration.
-    """
-    days = np.unique(df['Date'])
-    CV_subset = np.random.choice(days, iter)
-
-    rmses = []
-    test_periods = []
-    train_periods = []
-    persistence_model = []
-    for day in CV_subset:
-
-        # cross validation training set
-        train_start = pd.to_datetime(day) + pd.Timedelta(f"-{train_duration} days")
-        train_end = pd.to_datetime(day)
-        mask1 = df['final_date'] >= pd.to_datetime(train_start)
-        mask2 = df['final_date'] < pd.to_datetime(train_end)
-        x_train, y_train = create_X_y(df[mask1 & mask2], columns)
-
-
-        # cross_validation testing set
-        test_end = pd.to_datetime(day) + pd.Timedelta(f"+{test_duration} days")
-        test_start = pd.to_datetime(day) + pd.Timedelta("+1 days")
-        mask3 = df['final_date'] >= pd.to_datetime(test_start)
-        mask4 = df['final_date'] < pd.to_datetime(test_end)
-        x_test, y_test = create_X_y(df[mask3 & mask4], columns)
-
-        #persistence_model_predictions
-        pers_mod_y_hat_test = df[mask3 & mask4]['Direct Normal [W/m^2]']
-
-        if network:
-            model.fit(x_train,
-                      y_train,
-                      epochs=network['epochs'],
-                      batch_size=network['batch_size'],
-                      shuffle=network['shuffle'],
-                      validation_split=network['validation_split'],
-                      # callbacks=[network['callback']],
-                      verbose=1)
-
-            y_hat = model.predict(x_test)
-        else:
-            model.fit(x_train, y_train)
-            y_hat = model.predict(x_test)
-
-        rmse_test_error = np.sqrt(mean_squared_error(y_test, y_hat))
-        pm_test_error = np.sqrt(mean_squared_error(y_test, pers_mod_y_hat_test))
-
-        print("{} RMSE: {:.4f} | Persistence Model RMSE: {:.4f}".format(model.__class__.__name__, rmse_test_error, pm_test_error))
-
-        rmses.append(rmse_test_error)
-        persistence_model.append(pm_test_error)
-        test_periods.append((test_start, test_end))
-        train_periods.append((train_start, train_end))
-
-    print("\nAverage RMSE over {} splits: {:.4f}".format(iter, np.mean(rmses)))
-
-    return rmses, test_periods, train_periods, persistence_model
+    ----------
+    mae : (float)
+        Testing Mean Absolute Error
+    scores : (dict)
+        Cross validation scores
+    '''
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.15)
+    scores = cross_validate(model, x_train, y_train, scoring='neg_mean_absolute_error', verbose=0, n_jobs=-1, cv=5)
+    model.fit(x_train, y_train)
+    y_hat = model.predict(x_test)
+    mae = mean_absolute_error(y_test, y_hat)
+    return mae, scores
 
 
 def error_plot(y_dict, colors, title, xlab, ylab, savefig=False):
@@ -494,139 +445,17 @@ def build_neural_network(n_predictors, hidden_layer_neurons):
 
 if __name__ == "__main__":
 
-    df = get_master_df("../data/ivanpah_measurements.csv")
+    df = format_nrel_dataframe("../data/2003_2016.csv")
+    lag_features = ['Temperature', 'Clearsky DHI', 'Clearsky DNI', 'Clearsky GHI', 'Cloud Type','Dew Point','DHI','DNI','Fill Flag', 'GHI','Relative Humidity','Solar Zenith Angle','Surface Albedo','Pressure','Precipitable Water','Wind Direction','Wind Speed']
+    df = create_lagged_features(df, lag_features, 4, 30)
+    df = create_future_target(df, 'DNI', 1, 30)
 
-    # drop un-used columns
-    df.drop(['PST',
-             'Global Horiz [W/m^2]',
-             'Global UVA [W/m^2]',
-             'Global UVE [W/m^2]',
-             'Global UVE [Index]',
-             'UVSAET Temp [deg C]',
-             'Logger Temp [deg C]',
-             'Logger Battery [VDC]',
-              'Diffuse Horiz (calc) [W/m^2]'], axis=1, inplace=True)
-
-    print("\nData successfully loaded")
-
-    df = create_lagged_DNI_features(15, df)
-
-    print("\n15 new features successfully engineered")
-
-    df = df[df['Direct Normal [W/m^2]'] > -10]
-
-    print("\nDataFrame limited to observation with DNI >= -10")
-
-    columns = ['Year',
-            'Month',
-            'DOY',
-            'Hour',
-            'Minute',
-            'Direct Normal [W/m^2]',
-            'Zenith Angle [degrees]',
-            'Azimuth Angle [degrees]',
-            'Airmass',
-            'Avg Wind Speed @ 30ft [m/s]',
-            'Avg Wind Direction @ 30ft [deg from N]',
-            'Peak Wind Speed @ 30ft [m/s]',
-            'Wind Chill Temp [deg C]',
-            'DNI_T_minus1',
-            'DNI_T_minus2',
-            'DNI_T_minus3',
-            'DNI_T_minus4',
-            'DNI_T_minus5',
-            'DNI_T_minus6',
-            'DNI_T_minus7',
-            'DNI_T_minus8',
-            'DNI_T_minus9',
-            'DNI_T_minus10',
-            'DNI_T_minus11',
-            'DNI_T_minus12',
-            'DNI_T_minus13',
-            'DNI_T_minus14',
-            'DNI_T_minus15']
-
-    test_dates = get_random_test_dates(5, 2017, 2)
-
-###############################################################################
-################################# BASE MODEL ##################################
-###############################################################################
+    test_dates = tmb.get_random_test_dates(5, 2017, (6,18), 2)
 
     rf = RandomForestRegressor()
 
-    print("\nStarting cross validation...\n")
+    X, y = create_X_y(df, df.columns, 'DNI_T_plus30', test_dates[0], 3, 'months', same=True)
 
-    cv_errors, cv_test_periods, cv_train_periods, pm_errors = test_model(rf, columns, 10, 90, 31, df)
-
-    print("\nCross validation complete.")
-
-    base_model_evaluation = zip(cv_train_periods, cv_test_periods, cv_errors, pm_errors)
-    base_model_overview = []
-    for triplet in base_model_evaluation:
-        base_model_overview.append([str(triplet[0][0]), str(triplet[0][1]), str(triplet[1][0]), str(triplet[1][1]), triplet[2], triplet[3]])
-    base_model_df = pd.DataFrame(base_model_overview, columns=['Train_Start','Train_End','Test_Start','Test_End','Test_Error','Persistent_Model_error'])
-
-    # Persistence Model
-    np.mean(np.sqrt(mean_squared_error(df['DNI_T_plus15'].values, df['Direct Normal [W/m^2]'].values)))
-
-    # create display data
-    mask = df['Date'] == '2015-06-11'
-    mask2 = df['Hour'] > 10
-    mask3 = df['Hour'] <= 11
-    display_data = df[mask & mask2 & mask3][['final_date','Direct Normal [W/m^2]','DNI_T_plus15']].head(61)
-    display_data = display_data.set_index(np.arange(display_data.shape[0]))
-
-    # used in Predicting_DNI.md
-    top = display_data.loc[:6,:]
-    bottom = display_data.loc[15:21,]
-    final_display = pd.concat([top, bottom])
-
-    # plot CV errors
-    error_dict = {"Random Forest Error": base_model_df["Test_Error"].values,
-                  "Persistence Model Error": base_model_df["Persistent_Model_error"].values
-    }
-    error_plot(error_dict, ['red','orange'], "Random Forest vs. Persistence Model Errors", "Cross Validation Period", r"$\frac{Watts}{Meter^2}$", "../images/cross_validation_plot.png")
-
-###############################################################################
-############################### NEURAL NETWORK ################################
-###############################################################################
-
-    np.random.seed(5)
-
-    mlp = build_neural_network(len(columns), [8, 12])
-
-    stop_criteria = EarlyStopping(monitor='val_loss', min_delta=0.005)
-
-    network_dict = {'epochs': 5,
-                    'batch_size': 250,
-                    'shuffle': True,
-                    'validation_split': 0.25
-                    # 'callback': stop_criteria
-    }
-
-    cv_errors, cv_test_periods, cv_train_periods, pm_errors = test_model(mlp, columns, 10, 90, 31, df, network_dict)
-
-
-    # plot Neural Network CV errors
-    error_dict = {"Neural Network Error": cv_errors,
-                  "Persistence Model Error": pm_errors
-    }
-
-    error_plot(error_dict, ['lightblue','orange'], "Neural Network vs. Persistence Model Errors", "Cross Validation Period", r"$\frac{Watts}{Meter^2}$", '../images/neural_network_cv_error.png')
-
-    # plot sample day
-    np.random.seed(8)
-
-    day = np.random.choice(np.unique(df['Date']))
-
-    mask = df['Date'] == day
-
-    x_example = df[mask]
-
-    y_true = df[mask]['DNI_T_plus15']
-
-    y_hat = mlp.predict(x_example[columns])
-
-    x_example['Neural_net_preds'] = y_hat
-
-    plot_day(day, [6, 16], ['DNI_T_plus15','Neural_net_preds'], r"$Hour$", r"$\frac{Watts}{Meter^2}$", x_example, "../images/example_day.png")
+    X.drop(['final_date','Date'], axis=1, inplace=True)
+    # X.set_index(np.arange(X.shape[0]), inplace=True)
+    mae, scores = test_model(rf, X, y)
